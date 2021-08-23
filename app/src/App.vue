fd8b8faa463c37e54b5464a484aa5863bcb49091
Janssen Schembri <janssen_schembri@hotmail.com>
2021-08-23 14:41:52 +0200

    Create Header

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <HelloWorld />
+  <Header />
+  <Body />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from "./components/Header.vue";
+import Body from "./components/Body.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Header,
+    Body,
   },
 };
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap");
+@import "./scss/_variables.scss";
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Nunito Sans", sans-serif;
+}
 </style>
