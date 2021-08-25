e3d0727f10a65b499e97cfe62427aa2354655194
Janssen Schembri <janssen_schembri@hotmail.com>
2021-08-25 18:39:50 +0200

    Install Axios and Call API

--- app/src/components/Body.vue
+++ app/src/components/Body.vue
@@ -7,11 +7,33 @@
 </template>
 
 <script>
+import axios from "axios";
 import SearchArea from "./Search_Area.vue";
 export default {
   name: "Body",
   components: {
     SearchArea,
+  },
+  data() {
+    return {
+      countries: [],
+    };
+  },
+  methods: {
+    async getCountries() {
+      try {
+        const response = await axios.get(
+          "https://restcountries.eu/rest/v2/all"
+        );
+        this.countries = response.data;
+        console.log(this.countries);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+  created() {
+    this.getCountries();
   },
 };
 </script>
